21232609d86c23a78cc3c13d7dd8c8ea088de904
birdmanoutman <birdmanoutman@163.com>
2024-02-19 19:25:13 +0800

    20240219_来自904PC的更新

--- autoFormatClassfication.py
+++ autoFormatClassfication.py
@@ -3,18 +3,13 @@ import os
 import shutil
 
 def load_file_types(file_path):
-    """功能：读取并解析一个JSON文件，该文件包含不同类型的文件扩展名及其描述。
-        输入：file_path，一个字符串，表示JSON文件的路径。
-        输出：字典，键是文件类型，值是包含扩展名和描述的字典列表。"""
+    """功能：读取并解析一个JSON文件，该文件包含不同类型的文件扩展名及其描述。"""
     with open(file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data.get("file_types", {})
 
-
 def build_extension_mapping(file_types):
-    """功能：将从JSON文件中读取的数据转换成一个扩展名到文件类型和描述的映射。
-        输入：file_types，字典，包含从JSON文件中读取的文件类型和扩展名信息。
-        输出：字典，键是文件扩展名（小写），值是一个包含文件类型和描述的字典。"""
+    """功能：将从JSON文件中读取的数据转换成一个扩展名到文件类型和描述的映射。"""
     extension_to_type = {}
     for file_type, extension_dicts in file_types.items():
         for extension_dict in extension_dicts:
@@ -26,11 +21,8 @@ def build_extension_mapping(file_types):
     return extension_to_type
 
 
-def classify_files(folder_path, extension_to_type):
-    """功能：遍历指定文件夹中的所有文件，根据文件扩展名将文件归类到子文件夹中。
-        输入：folder_path：一个字符串，表示要分类文件的文件夹路径。
-            extension_to_type：一个字典，键是文件扩展名（小写），值是一个包含文件类型和描述的字典。
-        输出：无直接输出，但函数会根据文件类型移动文件，并在控制台上打印移动操作的信息。"""
+def classify_files(folder_path, target_folder_path, extension_to_type):
+    """功能：遍历指定文件夹中的所有文件，根据文件扩展名将文件归类到子文件夹中。"""
     files = os.listdir(folder_path)
     for file in files:
         source_path = os.path.join(folder_path, file)
@@ -38,7 +30,7 @@ def classify_files(folder_path, extension_to_type):
             file_extension = file.split('.')[-1].lower()
             if file_extension in extension_to_type:
                 file_info = extension_to_type[file_extension]
-                target_folder = os.path.join(folder_path, file_info["type"])
+                target_folder = os.path.join(target_folder_path, file_info["type"])
                 if not os.path.exists(target_folder):
                     os.makedirs(target_folder)
                 target_path = os.path.join(target_folder, file)
@@ -51,7 +43,8 @@ if __name__ == "__main__":
 
     folder_path = input(f"Enter the folder path (default: {default_folder_path}): ") or default_folder_path
     json_file_path = input(f"Enter the JSON file path (default: {default_json_file_path}): ") or default_json_file_path
+    target_folder_path = input(f"Enter the target folder path (default: same as folder path): ") or folder_path
 
     file_types = load_file_types(json_file_path)
     extension_to_type = build_extension_mapping(file_types)
-    classify_files(folder_path, extension_to_type)
+    classify_files(folder_path, target_folder_path, extension_to_type)
