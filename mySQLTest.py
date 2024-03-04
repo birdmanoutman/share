f642bcf99154e29bac661ed1f7cef0415afbf0df
birdmanoutman <birdmanoutman@163.com>
2024-03-05 00:30:42 +0800

    20240303_继续优化，主要是优化了文件自动归类和重命名策略，但是还是有一些bug，比如移动子文件夹中的文本文件到TXT文件时，会自动在TXT文件夹中建立对应的文件夹

--- mySQLTest.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-# Replace with your MySQL configuration
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="birdmanoutman",
-    password="Zez",
-    database="your_database"
-)
